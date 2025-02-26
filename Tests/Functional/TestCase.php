71376782c5f9170bcbf885eaaf5b542a1f828826
AntoineLemaire <antoine@bouffe.io>
2025-02-26 14:47:46 +0100

    Upgrade to Symfony4

--- Tests/Functional/TestCase.php
+++ Tests/Functional/TestCase.php
@@ -21,7 +21,7 @@ abstract class TestCase extends WebTestCase
     /**
      * {@inheritdoc}
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         $fs = new Filesystem();
         $fs->remove(sys_get_temp_dir().'/DataLayerBundle/');
@@ -30,7 +30,7 @@ abstract class TestCase extends WebTestCase
     /**
      * {@inheritdoc}
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         static::$kernel = null;
     }
